709fa19bbb9a0cbc8fc3c0e4986aa5a97edf3675
Aimee Galang <aimeegalang@gmail.com>
2021-09-11 20:05:32 +0000

    first commit

---
+++ static/ajax-exercise.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+  fetch("/fortune")
+  .then((response) => response.text())
+  .then((serverData) => {
+    document.querySelector("#fortune-text").innerHTML = serverData;
+  });
+}
+
+document
+  .querySelector("#get-fortune-button")
+  .addEventListener("click", showFortune);
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+  evt.preventDefault();
+
+  const url = "/weather.json";
+  const zipcode = document.querySelector("#zipcode-field").value;
+
+  const queryString = new URLSearchParams({zipcode}).toString();
+  console.log({zipcode});
+
+  fetch(`/weather.json?${queryString}`)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    document.querySelector("#weather-info").innerText = responseJson.forecast;
+  });
+  
+
+  // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+document
+  .querySelector("#weather-form")
+  .addEventListener("submit", showWeather);
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+  evt.preventDefault();
+
+  // TODO: show the result message after your form
+  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+document.querySelector("#order-form").addEventListener("submit", orderMelons);
